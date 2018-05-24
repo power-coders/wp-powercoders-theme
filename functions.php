9750219efea2231f6ceedefc22700ddc8594cf91
Marwa Ibish <38419617+memoibish@users.noreply.github.com>
2018-05-24 13:37:47 +0200

    person post type

--- functions.php
+++ functions.php
@@ -37,5 +37,21 @@ function powercoders_post_types(){
             ),
         'menu_icon' =>'dashicons-admin-users'
     ));
-    add_action('init','university_post_types');
+       //Program post type
+     register_post_type('program',array(
+        'supports' => array('title', 'editor'),
+        'rewrite' => array('slug' => 'programs'),
+        'has_archive' => true,      
+        'public' => true,
+        'labels' => array(
+            'name' => 'programs',
+            'add_new_item' => 'Add New Program',
+            'edit_item' => 'Edit Program',
+            'all_items' => 'All Programs',
+            'singular_name' => 'Program'
+            ),
+        'menu_icon' =>'dashicons-awards'
+    ));
 }
+    add_action('init','university_post_types');
+
