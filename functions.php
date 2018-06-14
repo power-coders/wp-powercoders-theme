0650394e067f89e6b5842c1698fd11bbbbe086d8
Marwa Ibish <marwa.ibish@flake.ch>
2018-06-14 13:11:18 +0200

    adding the header section with logo image

--- functions.php
+++ functions.php
@@ -3,20 +3,80 @@
 function powercoders_setup_theme(){
     add_theme_support('title-tag');         // https://codex.wordpress.org/Title_Tag
     add_theme_support('post-thumbnails');   // https://codex.wordpress.org/Post_Thumbnails
+    add_theme_support('menus');             // Add Menu Support
+
+    // Add Thumbnail Theme Support
+    add_theme_support('post-thumbnails');
+    add_image_size('large', 700, '', true); // Large Thumbnail
+    add_image_size('medium', 250, '', true); // Medium Thumbnail
+    add_image_size('small', 120, '', true); // Small Thumbnail
+    add_image_size('custom-size', 700, 200, true); // Custom Thumbnail Size call using the_post_thumbnail('custom-size');
 
     register_nav_menu('main', 'Main Menu'); // we will be using one menu for navigation
 }
+
 add_action('after_setup_theme', 'powercoders_setup_theme');
 
 function powercoders_enqueue_scripts()
 {
     // https://codex.wordpress.org/Plugin_API/Action_Reference/wp_enqueue_scripts
+    wp_enqueue_style( 'style.css', get_template_directory_uri() . '/style.css', array() );
 }
 add_action('wp_enqueue_scripts', 'powercoders_enqueue_scripts');
 
 function powercoders_widgets_init()
 {
     // https://codex.wordpress.org/Widgets_API
+
+    register_sidebar( array(
+      'name' => 'Content Footer', 'powercoders',
+      'id' => 'content-footer',
+      'description' => 'Appears in the footer area',
+      'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+      'after_widget' => '</aside>',
+      'before_title' => '<h3 class="widget-title">',
+      'after_title' => '</h3>',
+    ) );
+
+    register_sidebar( array(
+      'name' => 'Get Involved Footer', 'powercoders',
+      'id' => 'get-involved-footer',
+      'description' => 'Appears in the footer area',
+      'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+      'after_widget' => '</aside>',
+      'before_title' => '<h3 class="widget-title">',
+      'after_title' => '</h3>',
+    ) );
+
+    register_sidebar( array(
+      'name' => 'Powercoders Footer', 'powercoders',
+      'id' => 'powercoders-footer',
+      'description' => 'Appears in the footer area',
+      'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+      'after_widget' => '</aside>',
+      'before_title' => '<h3 class="widget-title">',
+      'after_title' => '</h3>',
+    ) );
+
+    register_sidebar( array(
+      'name' => 'Mailinglist Footer', 'powercoders',
+      'id' => 'mailinglist-footer',
+      'description' => 'Appears in the footer area',
+      'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+      'after_widget' => '</aside>',
+      'before_title' => '<h3 class="widget-title">',
+      'after_title' => '</h3>',
+    ) );
+
+    register_sidebar( array(
+      'name' => 'Donate Footer', 'powercoders',
+      'id' => 'donate-footer',
+      'description' => 'Appears in the footer area',
+      'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+      'after_widget' => '</aside>',
+      'before_title' => '<h3 class="widget-title">',
+      'after_title' => '</h3>',
+    ) );
 }
 add_action('widgets_init', 'powercoders_widgets_init');
 
