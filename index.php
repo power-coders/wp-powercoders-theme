d79a81a24708ac4890751d884bbf84d6478b3b80
Marwa Ibish <marwa.ibish@flake.ch>
2018-07-05 14:19:29 +0200

    changes from Hüni

--- index.php
+++ index.php
@@ -1,43 +1,45 @@
 <?php /* Default Page Template */ ?>
 
 <?php
-    get_header(); 
+ get_header(); 
 ?>
 <?php
-    while (have_posts()) : the_post(); { 
+ while (have_posts()) : the_post(); { 
 ?>
-<div class="parallax-container valign-wrapper">
-    <div class="parallax">
-        <!--show hero image -->
-        <?php $hero = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'full' ); ?>
-        <!-- get the alt image else take the title as a alt of the image-->
-        <?php if ($alt=get_the_post_thumbnail_caption()){
-    
-              } else {
-                    $alt = get_the_title();
-              }
-        ?>
-        <img src="<?php echo $hero['0'];?>" alt="<?php  echo $alt?> "></div>
-</div>
-<h1 class="center">    
-    <?php  echo the_title();?>
-</h1>
-
-<div class="container">
-    <div class="row">
-<div class="col s12 offset-l2 l8">
-
-        <?php  the_content();?>
-</div>
-    </div>
-</div>
-
-
-
+        <div class="parallax-container valign-wrapper">
+            <div class="parallax">
+                <!--show hero image -->
+                <?php $hero = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'full' ); ?>
+                <!-- get the alt image else take the title as a alt of the image-->
+                <?php 
+                     $alt=get_the_post_thumbnail_caption();
+                     if ($alt != get_the_post_thumbnail_caption()){
+                        $alt = get_the_title();
+                    } 
+                 ?>
+                 
+                 
+                 
+                <img src="<?php echo $hero['0'];?>" alt="<?php echo $alt?> "></div>
+        </div>
+        <h1 class="center">
+            <?php  echo the_title();?>
+        </h1>
+        <div class="container">
+            <div class="row">
+                <div class="col s12 offset-l2 l8">
+                    <?php  the_content();?>
+                </div>
+            </div>
+        </div>
 
 
-<?php }
+
+
+
+<?php 
 endwhile;
 ?>
-
-<?php get_footer(); ?>
+<?php
+get_footer();
+?>
